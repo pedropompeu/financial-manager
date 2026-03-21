a6031d6e245910133548b32d29c966af7611fa4b
Your Name <you@example.com>
2026-03-21 20:31:01 -0300

    feat: add transactions CRUD endpoint

--- backend/apps/transactions/urls.py
+++ backend/apps/transactions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoriaViewSet
+from .views import CategoriaViewSet, TransacaoViewSet
 
 router = DefaultRouter()
 router.register('categorias', CategoriaViewSet, basename='categoria')
+router.register('transacoes', TransacaoViewSet, basename='transacao')
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/apps/transactions/views.py
+++ backend/apps/transactions/views.py
@@ -7,3 +7,8 @@ from .serializers import CategoriaSerializer, TransacaoSerializer
 class CategoriaViewSet(FiltroOrganizacaoMixin, viewsets.ModelViewSet):
     serializer_class = CategoriaSerializer
     queryset = Categoria.objects.all()
+
+
+class TransacaoViewSet(FiltroOrganizacaoMixin, viewsets.ModelViewSet):
+    serializer_class = TransacaoSerializer
+    queryset = Transacao.objects.select_related('categoria')
